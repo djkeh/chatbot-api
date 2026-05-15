60d82dc9723bcfa6dd883650725f188ca56238b0
Uno Kim <djkehh@gmail.com>
2026-05-15 21:41:50 +0900

    Initial commit

---
+++ src/test/kotlin/me/uno/chatbotapi/ChatbotApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.uno.chatbotapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatbotApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
