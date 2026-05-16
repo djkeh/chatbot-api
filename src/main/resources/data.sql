2459f22460684a48a00a3d045b639938e8bf68b3
Uno Kim <djkehh@gmail.com>
2026-05-16 14:13:11 +0900

    fix: `data.sql` 테스트 암호 수정
    잘못된 암호화 문자열이었던 것 같다. 수정함.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 -- Initial admin user: admin@email.com / password123!
 INSERT INTO user_account (email, password, name, role, created_at, updated_at)
-VALUES ('admin@email.com', '{bcrypt}$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRzgVymGe07xd00pdRQXhr9B87i', 'Administrator', 'ADMIN', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+VALUES ('admin@email.com', '{bcrypt}$2a$10$6.8WY2DvLzvhrFjNaveH/uqVVs1cTDC.776El6YReGOInli4bLgMy', 'Administrator', 'ADMIN', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
 ;
